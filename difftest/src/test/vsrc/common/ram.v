4c6c8a0c1e5e13c868c91ce7b3cf0cc1466a0ef2
tracer-oscpu2021 <tracer@oscpu.org>
2021-08-13 12:17:33 +0800

    杨煜 20210092    Static hostname: yyang-HLY-WX9XX          Icon name: computer-laptop            Chassis: laptop         Machine ID: d79aa8e3f8a44d5e81b9f3a249e0c8d0            Boot ID: 9c6cb497fe8f4d63b611ed1645fa3874   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-25-generic       Architecture: x86-64  12:17:33 up  1:34,  1 user,  load average: 1.76, 1.79, 1.92

--- difftest/src/test/vsrc/common/ram.v
+++ difftest/src/test/vsrc/common/ram.v
@@ -1,51 +1,18 @@
-// /***************************************************************************************
-// * Copyright (c) 2020-2021 Institute of Computing Technology, Chinese Academy of Sciences
-// * Copyright (c) 2020-2021 Peng Cheng Laboratory
-// *
-// * XiangShan is licensed under Mulan PSL v2.
-// * You can use this software according to the terms and conditions of the Mulan PSL v2.
-// * You may obtain a copy of Mulan PSL v2 at:
-// *          http://license.coscl.org.cn/MulanPSL2
-// *
-// * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-// * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-// * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-// *
-// * See the Mulan PSL v2 for more details.
-// ***************************************************************************************/
-
-// import "DPI-C" function void ram_write_helper
-// (
-//   input  longint    wIdx,
-//   input  longint    wdata,
-//   input  longint    wmask,
-//   input  bit        wen
-// );
-
-// import "DPI-C" function longint ram_read_helper
-// (
-//   input  bit        en,
-//   input  longint    rIdx
-// );
-
-// module RAMHelper(
-//   input         clk,
-//   input         en,
-//   input  [63:0] rIdx,
-//   output [63:0] rdata,
-//   input  [63:0] wIdx,
-//   input  [63:0] wdata,
-//   input  [63:0] wmask,
-//   input         wen
-// );
-
-//   assign rdata = ram_read_helper(en, rIdx);
-
-//   always @(posedge clk) begin
-//     ram_write_helper(wIdx, wdata, wmask, wen && en);
-//   end
-
-// endmodule
+/***************************************************************************************
+* Copyright (c) 2020-2021 Institute of Computing Technology, Chinese Academy of Sciences
+* Copyright (c) 2020-2021 Peng Cheng Laboratory
+*
+* XiangShan is licensed under Mulan PSL v2.
+* You can use this software according to the terms and conditions of the Mulan PSL v2.
+* You may obtain a copy of Mulan PSL v2 at:
+*          http://license.coscl.org.cn/MulanPSL2
+*
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+*
+* See the Mulan PSL v2 for more details.
+***************************************************************************************/
 
 import "DPI-C" function void ram_write_helper
 (
@@ -61,34 +28,21 @@ import "DPI-C" function longint ram_read_helper
   input  longint    rIdx
 );
 
-
-module ram_mem(
-    input                       clk,
-    
-    input   [63:0]    inst_addr,
-    input                       inst_en,
-    output  [63:0]         inst,
-
-    // DATA PORT
-    input                       mem_write,
-    input                       mem_read,
-    input   [63:0]         write_mask,
-    input   [63:0]    data_addr,
-    input   [63:0]         write_data,
-    output  [63:0]         read_data
+module RAMHelper(
+  input         clk,
+  input         en,
+  input  [63:0] rIdx,
+  output [63:0] rdata,
+  input  [63:0] wIdx,
+  input  [63:0] wdata,
+  input  [63:0] wmask,
+  input         wen
 );
 
-    // INST PORT
+  assign rdata = ram_read_helper(en, rIdx);
 
-    wire [63:0] inst_pre= ram_read_helper(inst_en,{3'b000,(inst_addr-64'h0000_0000_8000_0000)>>3});
+  always @(posedge clk) begin
+    ram_write_helper(wIdx, wdata, wmask, wen && en);
+  end
 
-    assign inst = inst_addr[2] ? inst_pre[63:32] : inst_pre[31:0];
-
-    // DATA PORT 
-    assign read_data = ram_read_helper(mem_read, {3'b000,(data_addr-64'h0000_0000_8000_0000)>>3});
-
-    always @(posedge clk) begin
-        ram_write_helper((data_addr-64'h0000_0000_8000_0000)>>3, write_data, write_mask, mem_write);
-    end
-    
 endmodule
