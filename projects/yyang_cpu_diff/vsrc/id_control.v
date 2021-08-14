ba9ed62dd6414e40092809346b7d11b644bf1387
tracer-oscpu2021 <tracer@oscpu.org>
2021-08-14 11:08:36 +0800

    杨煜 20210092    Static hostname: yyang-HLY-WX9XX          Icon name: computer-laptop            Chassis: laptop         Machine ID: d79aa8e3f8a44d5e81b9f3a249e0c8d0            Boot ID: 9c6cb497fe8f4d63b611ed1645fa3874   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-25-generic       Architecture: x86-64  11:08:36 up 1 day, 25 min,  1 user,  load average: 1.87, 1.95, 1.63

--- projects/yyang_cpu_diff/vsrc/id_control.v
+++ projects/yyang_cpu_diff/vsrc/id_control.v
@@ -263,11 +263,11 @@ module id_control (
                                    | ( { 3 { inst_bgeu } } & 3'b111 );          
 
     /* shift */
-    assign shift_type   =   /* 3'b000 | */ ( { 3 { inst_sll | inst_slli | inst_sllw | inst_slliw } } & 3'b011 )
-                                   /* | ( { 3 { inst_srlw | inst_srliw } } & 3'b111 )
+    assign shift_type   =   3'b000 | ( { 3 { inst_sll | inst_slli | inst_sllw | inst_slliw } } & 3'b011 )
+                                   | ( { 3 { inst_srlw | inst_srliw } } & 3'b111 )
                                    | ( { 3 { inst_srl | inst_srli } } & 3'b101 )         
                                    | ( { 3 { inst_sraw | inst_sraiw} } & 3'b110 )
-                                   | ( { 3 { inst_sra | inst_srai } } & 3'b100 ) */;
+                                   | ( { 3 { inst_sra | inst_srai } } & 3'b100 );
     assign shift_num_src=   1'b0 | ( inst_shift & inst_al_i );
 
     /* store */
