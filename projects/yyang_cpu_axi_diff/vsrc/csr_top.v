205af308d63cc20b949246e4dde9217d7c5420f2
tracer-oscpu2021 <tracer@oscpu.org>
2021-09-10 12:15:14 +0800

    杨煜 20210092    Static hostname: yyang-HLY-WX9XX          Icon name: computer-laptop            Chassis: laptop         Machine ID: d79aa8e3f8a44d5e81b9f3a249e0c8d0            Boot ID: ee2a1078b3414e6dbeea367dfa848efc   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  12:15:15 up 3 days, 22:18,  1 user,  load average: 2.28, 2.35, 2.35

--- projects/yyang_cpu_axi_diff/vsrc/csr_top.v
+++ projects/yyang_cpu_axi_diff/vsrc/csr_top.v
@@ -286,7 +286,7 @@ module csr_top (
 
 
     //output
-    assign csr_trap             =   mstatus_r[4] & ( mie_r[7] & mip_r[7] );
+    assign csr_trap             =   mstatus_r[3] & ( mie_r[7] & mip_r[7] );
     assign csr_nxt_pc           =   inst_trap ? mtvec_r: mepc_r;
     assign csr_read             =    { `DATA_BUS_SIZE { index_mcycle } } & ( mcycle_r )
                                     | { `DATA_BUS_SIZE{ index_misa } } & ( misa_r )
