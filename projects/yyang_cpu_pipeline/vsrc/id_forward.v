edc66692fa0bb77660b3df8b938f512d9e8a7cce
tracer-oscpu2021 <tracer@oscpu.org>
2021-10-09 12:49:43 +0800

    杨煜 20210092    Static hostname: yyang-HLY-WX9XX          Icon name: computer-laptop            Chassis: laptop         Machine ID: d79aa8e3f8a44d5e81b9f3a249e0c8d0            Boot ID: 64c7b33585ea4e108a3d8ba2b3f740be   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  12:49:43 up 1 day,  2:45,  1 user,  load average: 1.75, 1.82, 2.29

--- projects/yyang_cpu_pipeline/vsrc/id_forward.v
+++ projects/yyang_cpu_pipeline/vsrc/id_forward.v
@@ -83,16 +83,16 @@ module id_forward (
     assign id_rs2_src_mem2wb_n              =   ~id_rs2_src_mem2wb;
     
     assign id_rs1_src_reg                   =   id_rs1_src_id2ex_n & id_rs1_src_ex2mem_n & id_rs1_src_mem2wb_n;
-    assign id_rs1_src_id2ex                 =     ( id2ex_rd_index != `REG_INDEX_SIZE'b0 )
-                                                & ( id_rs1_index == id2ex_rd_index ) & id_rs1_en & id2ex_rd_en;
+    assign id_rs1_src_id2ex                 =   ( id2ex_rd_index != `REG_INDEX_SIZE'b0 ) & ( id_rs1_index == id2ex_rd_index )
+                                                & id_rs1_en & id2ex_rd_en;
     assign id_rs1_src_ex2mem                =   id_rs1_src_id2ex_n & ( ex2mem_rd_index != `REG_INDEX_SIZE'b0 )
                                                 & ( id_rs1_index == ex2mem_rd_index )& id_rs1_en & ex2mem_rd_en;
     assign id_rs1_src_mem2wb                =   id_rs1_src_id2ex_n & id_rs1_src_ex2mem_n & ( mem2wb_rd_index != `REG_INDEX_SIZE'b0 )
                                                 & ( id_rs1_index == mem2wb_rd_index )& id_rs1_en & mem2wb_rd_en;
     
     assign id_rs2_src_reg                   =   id_rs2_src_id2ex_n & id_rs2_src_ex2mem_n & id_rs2_src_mem2wb_n;
-    assign id_rs2_src_id2ex                 =     ( id2ex_rd_index != `REG_INDEX_SIZE'b0 )
-                                                & ( id_rs2_index == id2ex_rd_index ) & id_rs2_en & id2ex_rd_en;
+    assign id_rs2_src_id2ex                 =   ( id2ex_rd_index != `REG_INDEX_SIZE'b0 ) & ( id_rs2_index == id2ex_rd_index ) 
+                                                & id_rs2_en & id2ex_rd_en;
     assign id_rs2_src_ex2mem                =   id_rs2_src_id2ex_n & ( ex2mem_rd_index != `REG_INDEX_SIZE'b0 )
                                                 & ( id_rs2_index == ex2mem_rd_index )& id_rs2_en & ex2mem_rd_en;
     assign id_rs2_src_mem2wb                =   id_rs2_src_id2ex_n & id_rs2_src_ex2mem_n & ( mem2wb_rd_index != `REG_INDEX_SIZE'b0 )
